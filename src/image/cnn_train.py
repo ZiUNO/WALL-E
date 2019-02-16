0fd40589a37c20a8a8fe0d4abdf5a08dda0a29e7
ZiUNO <331245059@qq.com>
2019-02-16 18:47:00 +0800

    Update: 卷积神经网络：修改输出label时输入，修改为label对应的holder README：更改文件名

--- src/image/cnn_train.py
+++ src/image/cnn_train.py
@@ -14,13 +14,15 @@ from utils import jpg_handle
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 size_12 = size_21 = 64
-size_2 = 64
+size_23 = size_32 = 128
+size_3 = 64
 size_attri_12 = size_attri_21 = 768
 size_attri_2 = 384
-size_label = 320
+size_label_12 = size_label_21 = 320
+size_label_2 = 640
 
 max_step = 1000
-batch_size = 128
+batch_size = 32
 attribute_holder = tf.placeholder(tf.float32, [batch_size, 30])
 image_holder = tf.placeholder(tf.float32, [batch_size, 64, 64, 3])
 label_holder = tf.placeholder(tf.int32, [batch_size, 230])
@@ -36,7 +38,9 @@ with tf.name_scope('Inputs'):
 #     r'G:\AI\zero\DatasetA_train_20180813\DatasetA_train_20180813\attribute_list.txt')
 # label_list = jpg_handle.get_label_list(r'G:\AI\zero\DatasetA_train_20180813\DatasetA_train_20180813\label_list.txt')
 
-# network structure
+"""
+Tensorflow network structure
+"""
 with tf.name_scope('Conv1'):
     kernel_size_1 = 5
     weight_1 = tf.Variable(tf.truncated_normal([kernel_size_1, kernel_size_1, 3, size_12], stddev=5e-2))
@@ -48,15 +52,24 @@ with tf.name_scope('Conv1'):
 
 with tf.name_scope('Conv2'):
     kernel_size_2 = 5
-    weight_2 = tf.Variable(tf.truncated_normal([kernel_size_2, kernel_size_2, size_21, size_2], stddev=5e-2))
+    weight_2 = tf.Variable(tf.truncated_normal([kernel_size_2, kernel_size_2, size_21, size_23], stddev=5e-2))
     kernel_2 = tf.nn.conv2d(norm_1, weight_2, [1, 1, 1, 1], padding='SAME')
-    bias_2 = tf.Variable(tf.constant(0.1, shape=[size_2]))
+    bias_2 = tf.Variable(tf.constant(0.1, shape=[size_23]))
     conv_2 = tf.nn.relu(tf.nn.bias_add(kernel_2, bias_2))
     norm_2 = tf.nn.lrn(conv_2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
     pool_2 = tf.nn.max_pool(norm_2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
+with tf.name_scope('Conv3'):
+    kernel_size_3 = 5
+    weight_3 = tf.Variable(tf.truncated_normal([kernel_size_3, kernel_size_3, size_32, size_3], stddev=5e-2))
+    kernel_3 = tf.nn.conv2d(pool_2, weight_3, [1, 1, 1, 1], padding='SAME')
+    bias_3 = tf.Variable(tf.constant(0.0, shape=[size_3]))
+    conv_3 = tf.nn.relu(tf.nn.bias_add(kernel_3, bias_3))
+    norm_3 = tf.nn.lrn(conv_3, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
+    pool_3 = tf.nn.avg_pool(norm_3, ksize=[1, 3, 3, 1], strides=[1,2, 2, 1], padding='SAME')
+
 with tf.name_scope('FCA1'):
-    reshape = tf.reshape(pool_2, [batch_size, -1])
+    reshape = tf.reshape(pool_3, [batch_size, -1])
     dim = reshape.get_shape()[1].value
     weight_attri_1 = tf.Variable(tf.truncated_normal([dim, size_attri_12], stddev=0.04))
     bias2_attri = tf.Variable(tf.constant(0.1, shape=[size_attri_12]))
@@ -72,15 +85,22 @@ with tf.name_scope('LogitsA'):
     bias4_attri = tf.Variable(tf.constant(0.0, shape=[30]))
     logits_attri = tf.add(tf.matmul(local_attri_2, weight_attri_logits), bias4_attri)
 
-with tf.name_scope('FCL'):
-    weight_label = tf.Variable(tf.truncated_normal([30, size_label], stddev=1 / 30.0))
-    bias_label = tf.Variable(tf.constant(0.0, shape=[size_label]))
-    local_label = tf.nn.relu(tf.add(tf.matmul(logits_attri, weight_label), bias_label))
+with tf.name_scope('FCL1'):
+    weight_label_1 = tf.Variable(tf.truncated_normal([30, size_label_12], stddev=1 / 30.0))
+    bias_label_1 = tf.Variable(tf.constant(0.0, shape=[size_label_12]))
+    # local_label_1 = tf.nn.relu(tf.add(tf.matmul(logits_attri, weight_label_1), bias_label_1))
+    # 更改为属性的holder进行测试
+    local_label_1 = tf.nn.relu(tf.add(tf.matmul(attribute_holder, weight_label_1), bias_label_1))
+
+with tf.name_scope('FCL2'):
+    weight_label_2 = tf.Variable(tf.truncated_normal([size_label_21, size_label_2], stddev=1 / 30.0))
+    bias_label_2 = tf.Variable(tf.constant(0.0, shape=[size_label_2]))
+    local_label_2 = tf.nn.relu(tf.add(tf.matmul(local_label_1, weight_label_2), bias_label_2))
 
 with tf.name_scope('LogitL'):
-    weight_logits_label = tf.Variable(tf.truncated_normal([size_label, 230], stddev=1 / float(size_label)))
+    weight_logits_label = tf.Variable(tf.truncated_normal([size_label_2, 230], stddev=1 / float(size_label_12)))
     bias_logits_label = tf.Variable(tf.constant(0.0, shape=[230]))
-    logits_label = tf.add(tf.matmul(local_label, weight_logits_label), bias_logits_label)
+    logits_label = tf.add(tf.matmul(local_label_2, weight_logits_label), bias_logits_label)
 
 with tf.name_scope('MeanA'):
     cross_entropy_attribute = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_attri, labels=attribute_holder)
@@ -97,7 +117,7 @@ with tf.name_scope('Loss'):
     loss = tf.add_n(tf.get_collection('loss'))
     tf.summary.scalar('loss', loss)
 
-train_op = tf.train.AdamOptimizer(0.1).minimize(loss)
+train_op = tf.train.AdamOptimizer(0.05).minimize(loss)
 with tf.name_scope('Prediction'):
     correct_prediction = tf.equal(tf.argmax(label_holder, 1), tf.argmax(logits_label, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -112,10 +132,10 @@ sess.run(train_iterator.initializer)
 
 for step in range(max_step):
     image_batch, attribute_batch, label_batch = sess.run([image_train, attributes_train, labels_train])
-    # if step % 10 == 0:
-    #     accu = accuracy.eval(
-    #         {image_holder: image_batch, attribute_holder: attribute_batch, label_holder: label_batch})
-    #     print('accuracy before = %.3f, ' % accu, end='')
+    if step % 10 == 0:
+        accu = accuracy.eval(
+            {image_holder: image_batch, attribute_holder: attribute_batch, label_holder: label_batch})
+        print('accuracy before = %.3f, ' % accu, end='')
     start_time = time.time()
     _, loss_value, loss_a_value, loss_l_value = sess.run([train_op, loss, loss_attribute, loss_label],
                                                          feed_dict={image_holder: image_batch,
